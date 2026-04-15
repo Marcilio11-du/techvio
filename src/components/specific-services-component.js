7458d0a69a22251467476f03fc6ebd70dd7d290a
marcilio11-du <marcilio11du@gmail.com>
2026-04-15 21:41:21 +0100

    feat: create the components that will be concret for representing each one of specific services, on trus-choose section

---
+++ src/components/specific-services-component.js
@@ -0,0 +1,28 @@
+class SpecificServiceComponent extends HTMLElement {
+    constructor() {
+        super();
+        this.attachShadow({ mode: 'open' });
+        
+        const text = this.getAttribute('text') || 'Default TextService';
+
+        this.shadowRoot.innerHTML = `
+            <style>
+                .specific-service {
+                        background-color: #ffffff;
+                        padding: 5px;
+                        box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+                        border-radius: 8px;
+                        width: 100%;
+                        border-left: 2px solid #3E57D0;
+                        margin-bottom: 20px;
+                    }
+            </style>
+
+            <div class="specific-service">
+                <p>${text}</p>
+            </div>
+        `;
+    }
+}
+
+customElements.define('specific-service', SpecificServiceComponent);
