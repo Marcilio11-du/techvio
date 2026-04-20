f79468490c9f66765c20e10693906ea13ca85a94
marcilio11-du <marcilio11du@gmail.com>
2026-04-20 23:35:02 +0100

    refactor: create the animated version of specific-services component

--- src/components/specific-services-component.js
+++ src/components/specific-services-component.js
@@ -3,18 +3,57 @@ class SpecificServiceComponent extends HTMLElement {
         super();
         this.attachShadow({ mode: 'open' });
         
-        const text = this.getAttribute('text') || 'Default TextService';
+        const text = this.getAttribute('text') || 'Default Service';
 
         this.shadowRoot.innerHTML = `
             <style>
+                :host {
+                    display: block;
+                    width: 100%;
+                }
+
                 .specific-service {
-                        background-color: #ffffff;
-                        box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
-                        border-radius: 8px;
-                        width: 100%;
-                        border-left: 2px solid #3E57D0;
-                        margin-bottom: 20px;
-                    }
+                    position: relative;
+                    z-index: 1;
+                    color: #404040;
+                    font-size: 16px;
+                    font-weight: 600;
+                    display: block;
+                    padding: 13px 15px;
+                    border-radius: 5px;
+                    border-left: 2px solid #7b68ee;
+                    background-color: #ffffff;
+                    box-shadow: -1px 3px 20px 0px rgb(82, 90, 101, 0.1);
+                    transition: 0.4s;
+                    cursor: pointer;
+                    overflow: hidden;
+                }
+
+                /* Animação: Esquerda para Direita */
+                .specific-service::before {
+                    content: "";
+                    position: absolute;
+                    top: 0;
+                    left: -100%; 
+                    width: 100%;
+                    height: 100%;
+                    background-color: #7b68ee;
+                    transition: all 0.4s ease;
+                    z-index: -1;
+                }
+
+                .specific-service p {
+                    margin: 0;
+                    transition: color 0.4s;
+                }
+
+                .specific-service:hover::before {
+                    left: 0;
+                }
+
+                .specific-service:hover p {
+                    color: #ffffff;
+                }
             </style>
 
             <div class="specific-service">
